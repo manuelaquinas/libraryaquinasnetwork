79ea42c1b9a897c9dbaa1d2d3a00c954861218ee
manuelaquinas <130670844+manuelaquinas@users.noreply.github.com>
2023-05-23 14:56:14 -0500

    Update
    Update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:4000/'
+axios.defaults.baseURL = 'http://5.78.91.226/serp/'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
